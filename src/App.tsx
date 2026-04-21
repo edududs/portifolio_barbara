e4ad2efb4cc6b5a658dff71d57941083f33417cd
Eduardo <dudulj15@gmail.com>
2026-04-21 13:27:04 -0300

    feat: implement scroll-to-top functionality on route change
    - Added ScrollToTopOnRouteChange component to reset scroll position when navigating between routes.
    - Integrated useEffect and useLocation hooks from react-router for effective scroll management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { BrowserRouter } from 'react-router'
+import { useEffect } from 'react'
+import { BrowserRouter, useLocation } from 'react-router'
 import AppRoutes from './AppRoutes'
+
+function ScrollToTopOnRouteChange() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTopOnRouteChange />
       <AppRoutes />
     </BrowserRouter>
   )
